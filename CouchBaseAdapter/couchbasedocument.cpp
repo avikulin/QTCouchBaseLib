04bcc4ef3fa0c516e90357f102e664024475d037
avikulin <asvikulin@mail.ru>
2016-08-21 23:03:55 +0300

    Some refining for the further development.

--- CouchBaseAdapter/couchbasedocument.cpp
+++ CouchBaseAdapter/couchbasedocument.cpp
@@ -13,9 +13,9 @@ CouchBaseDocument::CouchBaseDocument(lcb_error_t error, const lcb_get_resp_t *re
     _key = QByteArray((const char*)resp->v.v0.key,resp->v.v0.nkey);
 
     _data = QByteArray((const char*)resp->v.v0.bytes, resp->v.v0.nbytes);
-    _cas = (*resp).v.v0.cas;
+    _cas = resp->v.v0.cas;
 
-    switch((*resp).v.v0.datatype){
+    switch(resp->v.v0.datatype){
         case LCB_VALUE_RAW:
                             _type = ContentType::rawBytes;
                             break;
@@ -28,7 +28,18 @@ CouchBaseDocument::CouchBaseDocument(lcb_error_t error, const lcb_get_resp_t *re
     }
 }
 
+CouchBaseDocument::CouchBaseDocument(QString key, QString data, ContentType type, QObject *parent)
+    :QObject(parent), _key{key}, _data{data}, _type{type}
+{
+
+}
+
 bool CouchBaseDocument::isValid()
 {
     return _validationStatus;
+}
+
+QString CouchBaseDocument::GetErrorInfo()
+{
+    return strerror(_error)
 }
